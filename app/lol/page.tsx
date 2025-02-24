347d1d958b938fae167ac904384ee76335486044
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-02-24 23:22:56 +0800

    add lol page

---
+++ app/lol/page.tsx
@@ -0,0 +1,10 @@
+import { Box, Button } from "@chakra-ui/react";
+import LiveList from "./live-list";
+
+export default function LoLMain() {
+  return (
+    <Box>
+      <LiveList />
+    </Box>
+  );
+}
